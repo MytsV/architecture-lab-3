de6482afd1a3cc1733897a6c376cb0696f880527
MytsV <mytsvictoria@gmail.com>
2023-05-01 21:21:57 +0300

    Refactor parser

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -2,6 +2,7 @@ package lang
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strings"
 
@@ -10,6 +11,7 @@ import (
 
 // Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
+	// Зберігає стан малюнку у спеціальній операції.
 	state painter.OperationState
 }
 
@@ -20,30 +22,46 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		commandLine := scanner.Text()
-		op := parse(commandLine, &p.state) // Отримати відповідну до команди операцію
-		if op != nil {
+		// Отримуємо відповідну до команди операцію ії тип.
+		op, status := parse(commandLine)
+		if op == nil || status == absent {
+			// Якщо операція не імплементована, видаємо помилку.
+			return nil, fmt.Errorf("Unknown command!")
+		} else if status == regular {
+			// Якщо операція звичайна, просто додаємо її у список до передачі в цикл.
 			res = append(res, op)
+		} else {
+			// Якщо операція впливає на стан, оновлюємо його.
+			p.state.Add(op)
 		}
 	}
+	// Завжди надсилаємо операцію зі станом у цикл подій, на першому місці.
 	res = append([]painter.Operation{p.state}, res...)
 
 	return res, nil
 }
 
-func parse(line string, state *painter.OperationState) painter.Operation {
-	fields := strings.Fields(line)
+type cmdType int32
+
+const (
+	regular  cmdType = 0
+	stateful cmdType = 1
+	absent   cmdType = 2
+)
+
+func parse(cmd string) (painter.Operation, cmdType) {
+	//Розділяємо строку на окремі текстові строки команди за пропусками.
+	fields := strings.Fields(cmd)
 	switch fields[0] {
 	case "white":
-		state.Add(painter.OperationFunc(painter.WhiteFill))
-		return nil
+		return painter.OperationFunc(painter.WhiteFill), stateful
 	case "green":
-		state.Add(painter.OperationFunc(painter.GreenFill))
-		return nil
+		return painter.OperationFunc(painter.GreenFill), stateful
 	case "rect":
-		return painter.OperationRect
+		return painter.OperationRect, regular
 	case "update":
-		return painter.UpdateOp
+		return painter.UpdateOp, regular
 	default:
-		panic("Unknown command!")
+		return nil, absent
 	}
 }
