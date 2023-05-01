134b6b61ab38b4aa3f7b7ac170fd8c10fee30595
MytsV <mytsvictoria@gmail.com>
2023-05-01 18:44:55 +0300

    Added logic of state commands and state independent ones

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -10,6 +10,7 @@ import (
 
 // Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
+	state painter.OperationState
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
@@ -19,20 +20,27 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		commandLine := scanner.Text()
-		op := parse(commandLine) // Отримати відповідну до команди операцію
-		res = append(res, op)
+		op := parse(commandLine, &p.state) // Отримати відповідну до команди операцію
+		if op != nil {
+			res = append(res, op)
+		}
 	}
+	res = append([]painter.Operation{p.state}, res...)
 
 	return res, nil
 }
 
-func parse(line string) painter.Operation {
+func parse(line string, state *painter.OperationState) painter.Operation {
 	fields := strings.Fields(line)
 	switch fields[0] {
 	case "white":
-		return painter.OperationFunc(painter.WhiteFill)
+		state.Add(painter.OperationFunc(painter.WhiteFill))
+		return nil
 	case "green":
-		return painter.OperationFunc(painter.GreenFill)
+		state.Add(painter.OperationFunc(painter.GreenFill))
+		return nil
+	case "rect":
+		return painter.OperationRect
 	case "update":
 		return painter.UpdateOp
 	default:
--- painter/op.go
+++ painter/op.go
@@ -1,7 +1,9 @@
 package painter
 
 import (
+	"image"
 	"image/color"
+	"image/draw"
 
 	"golang.org/x/exp/shiny/screen"
 )
@@ -20,6 +22,26 @@ func (ol OperationList) Do(t screen.Texture) (ready bool) {
 		ready = o.Do(t) || ready
 	}
 	return
+}
+
+type OperationState struct {
+	backgroundOperation Operation
+}
+
+func (os OperationState) Do(t screen.Texture) (ready bool) {
+	if os.backgroundOperation != nil {
+		os.backgroundOperation.Do(t)
+	}
+	return false
+}
+
+func (os *OperationState) Add(o Operation) {
+	switch o.(type) {
+	case OperationFunc:
+		os.backgroundOperation = o
+	default:
+		panic("Unknown operation")
+	}
 }
 
 // UpdateOp операція, яка не змінює текстуру, але сигналізує, що текстуру потрібно розглядати як готову.
@@ -45,4 +67,14 @@ func WhiteFill(t screen.Texture) {
 // GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+}
+
+var OperationRect = operationRect{}
+
+type operationRect struct{}
+
+func (o operationRect) Do(t screen.Texture) bool {
+	rect := image.Rect(50, 50, 100, 100)
+	t.Fill(rect, color.RGBA{R: 0xff, A: 0xff}, draw.Src)
+	return false
 }
